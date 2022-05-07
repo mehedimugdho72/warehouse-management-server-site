c8f2af8146a56965148664a6c8a93e01f321b7a6
rana135 <rana286090@gmail.com>
2022-05-07 23:02:23 +0600

    specific One item Delete

--- index.js
+++ index.js
@@ -17,20 +17,34 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("star").collection("product");
-        app.get('/product', async(req, res) => {
+        // get product items:-
+        app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         })
+        // single product showing:-
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
         })
-    
-    } 
+        // Product Added by Form:-
+        app.post('/product', async (req, res) => {
+            const myItems = req.body;
+            const result = await productCollection.insertOne(myItems);
+            res.send(result);
+        })     
+        // specific One item Delete:-
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+      })
+    }
     finally {
         // await client.close();
     }
