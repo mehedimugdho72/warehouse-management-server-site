f11e160583fd448524688dea82465437529690de
rana135 <rana286090@gmail.com>
2022-05-09 04:34:32 +0600

    updated done

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorize access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    });
+    // console.log('inside verifyingJWT', authHeader)
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v0kir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,6 +35,16 @@ async function run() {
         await client.connect();
         const productCollection = client.db("star").collection("product");
         const reviewsCollection = client.db("starReview").collection("review");
+
+        // Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
+
         // get product items:-
         app.get('/product', async (req, res) => {
             const query = {};
@@ -52,7 +79,34 @@ async function run() {
             const result = await productCollection.deleteOne(query);
             res.send(result);
         })
-
+        // find my item
+        app.get('/products', verifyJWT, async (req, res) => {
+            const decodeEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodeEmail) {
+                const query = { email: email }
+                const cursor = productCollection.find(query);
+                const result = await cursor.toArray()
+                res.send(result)
+            }
+            else {
+                res.send(403).send({ message: 'forbiden access' })
+            }
+        })
+        // Update item:-
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateUser = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateUser.quantity
+                }
+            }
+            const result = await productCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
     }
     finally {
         // await client.close();
