70c741720159c0a6b47fbe27ec00a87f13cb2a51
rana135 <rana286090@gmail.com>
2022-05-09 23:31:50 +0600

    done

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unauthorize access' })
-    }
-    const token = authHeader.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access' })
-        }
-        console.log('decoded', decoded);
-        req.decoded = decoded;
-        next();
-    });
-    console.log('inside verifyingJWT', authHeader)
-    next();
-}
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorize access' })
+//     }
+//     const token = authHeader.split(' ')[1]
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden access' })
+//         }
+//         console.log('decoded', decoded);
+//         req.decoded = decoded;
+//         next();
+//     });
+//     console.log('inside verifyingJWT', authHeader)
+//     next();
+// }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v0kir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
