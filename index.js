45786dcd096c41d6b0a6dc6d8b7d8de1e12aea62
rana135 <rana286090@gmail.com>
2022-05-09 22:20:30 +0600

    Alhamdulillah done

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'unauthorize access' })
-//     }
-//     const token = authHeader.split(' ')[1]
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden access' })
-//         }
-//         console.log('decoded', decoded);
-//         req.decoded = decoded;
-//         next();
-//     });
-//     console.log('inside verifyingJWT', authHeader)
-//     next();
-// }
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorize access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    });
+    console.log('inside verifyingJWT', authHeader)
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v0kir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
@@ -80,19 +80,19 @@ async function run() {
             res.send(result);
         })
         // find my item
-        app.get('/product', async (req, res) => {
+        app.get('/product',verifyJWT, async (req, res) => {
             const email = req.query.email;
-            // if (email) {
+            if (email) {
                 const query = { email: email }
                 const cursor = productCollection.find(query);
                 const result = await cursor.toArray()
                 res.send(result)
-            // }
-            // else {
-            //     res.send(403).send({ message: 'forbiden access' })
-            // }
+            }
+            else {
+                res.send(403).send({ message: 'forbiden access' })
+            }
         })
-        // Update item:-
+        // Update item Added:-
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
             const updateUser = req.body;
